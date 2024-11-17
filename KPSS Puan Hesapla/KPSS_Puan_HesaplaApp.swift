5447d45d7b447f98d40652373e3a49a735672112
Ahmet Hakan YILDIRIM <ahakan.yildirim336@gmail.com>
2024-11-17 19:35:31 +0300

    initialized project

---
+++ KPSS Puan Hesapla/KPSS_Puan_HesaplaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPSS_Puan_HesaplaApp.swift
+//  KPSS Puan Hesapla
+//
+//  Created by AHMET HAKAN YILDIRIM on 17.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct KPSS_Puan_HesaplaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
