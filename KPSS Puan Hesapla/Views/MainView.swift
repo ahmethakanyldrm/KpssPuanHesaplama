ba14cc86517f5ead4eb1f9ed2b7b120028c0f90a
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2024-11-17 22:07:14 +0300

    Sayfa tasarlandı. Listview ile listeler oluşturuldu. NavigationLink ile yönlendirmeler yapıldı.

--- KPSS Puan Hesapla/Views/MainView.swift
+++ KPSS Puan Hesapla/Views/MainView.swift
@@ -9,7 +9,99 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                List {
+                    Section {
+                        NavigationLink(destination: SecondaryEducationView()) {
+                            HStack {
+                                Image(systemName: "1.circle")
+                                    .resizable()
+                                    .foregroundStyle(.blue)
+                                    .frame(width: 30, height: 30)
+                                
+                                Text("Ortaöğretim")
+                                    .badge(
+                                     Text("P94")
+                                        .italic()
+                                        .foregroundStyle(.main)
+                                    )
+                                
+                            }
+                        }
+                        NavigationLink(destination: AssociateView()) {
+                            HStack {
+                                Image(systemName: "2.circle")
+                                    .resizable()
+                                    .foregroundStyle(.blue)
+                                    .frame(width: 30, height: 30)
+                                
+                                Text("Önlisans")
+                                    .badge(
+                                     Text("P93")
+                                        .italic()
+                                        .foregroundStyle(.main)
+                                    )
+                                
+                            }
+                        }
+                        NavigationLink(destination: LicenseView()) {
+                            HStack {
+                                Image(systemName: "3.circle")
+                                    .resizable()
+                                    .foregroundStyle(.blue)
+                                    .frame(width: 30, height: 30)
+                                
+                                Text("Lisans (B Grubu)")
+                                    .badge(
+                                     Text("P3")
+                                        .italic()
+                                        .foregroundStyle(.main)
+                                    )
+                                
+                            }
+                        }
+                        NavigationLink(destination: EducationalSciencesView()) {
+                            HStack {
+                                Image(systemName: "4.circle")
+                                    .resizable()
+                                    .foregroundStyle(.blue)
+                                    .frame(width: 30, height: 30)
+                                
+                                Text("Eğitim Bilimleri")
+                                    .badge(
+                                     Text("P10")
+                                        .italic()
+                                        .foregroundStyle(.main)
+                                    )
+                                
+                            }
+                        }
+                        NavigationLink(destination: OABTView())  {
+                            HStack {
+                                Image(systemName: "5.circle")
+                                    .resizable()
+                                    .foregroundStyle(.blue)
+                                    .frame(width: 30, height: 30)
+                                
+                                Text("ÖABT")
+                                    .badge(
+                                     Text("P121")
+                                        .italic()
+                                        .foregroundStyle(.main)
+                                    )
+                                
+                            }
+                        }
+                    } header: {
+                        Text("Bölüm Seçiniz")
+                            .textCase(.none)
+                    }
+                    
+                }
+            }
+            .navigationTitle("KPSS Puan Hesaplama")
+        }
     }
 }
 
