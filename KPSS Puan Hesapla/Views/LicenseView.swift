a207214010ece5ef3f672238b1adf5ab92226f10
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2024-11-17 23:22:53 +0300

    lisans sayfası tasarlandı ve hesaplama işlemine animasyon eklendi.

--- KPSS Puan Hesapla/Views/LicenseView.swift
+++ KPSS Puan Hesapla/Views/LicenseView.swift
@@ -9,11 +9,96 @@ import SwiftUI
 
 // Lisans
 struct LicenseView: View {
+    @State private var gkTrueCount: Double = 60
+    @State private var gkFalseCount: Double = 0
+    
+    @State private var gyTrueCount: Double = 30
+    @State private var gyFalseCount: Double = 0
+    
+    @State private var result2022: Double = 0
+    @State private var result2023: Double = 0
+    
     var body: some View {
         VStack {
             
+            Form {
+                Section {
+                    Stepper("Doğru Sayısı: \(gkTrueCount, specifier: "%.0f")", value: $gkTrueCount, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gkTrueCount)
+                        .bold()
+                    Stepper("Yanlış Sayısı: \(gkFalseCount, specifier: "%.0f")", value: $gkFalseCount, in: 0...60)
+                        .sensoryFeedback(.selection, trigger: gkFalseCount)
+                        .bold()
+                    
+                } header: {
+                    Text("Genel Kültür")
+                        .textCase(.none)
+                }footer: {
+                    if(gkTrueCount + gkFalseCount > 60) {
+                        Text("Toplam doğru ve yanlış sayıları 60'ı geçemez")
+                            .foregroundStyle(.red)
+                    }
+                }
+                
+                Section {
+                    Stepper("Doğru Sayısı: \(gyTrueCount, specifier: "%.0f")", value: $gyTrueCount, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gyTrueCount)
+                        .bold()
+                    Stepper("Yanlış Sayısı: \(gyFalseCount, specifier: "%.0f")", value: $gyFalseCount, in: 0...60)
+                        .sensoryFeedback(.selection, trigger: gyFalseCount)
+                        .bold()
+                    
+                } header: {
+                    Text("Genel Yetenek")
+                        .textCase(.none)
+                }footer: {
+                    if(gyTrueCount + gyFalseCount > 60) {
+                        Text("Toplam doğru ve yanlış sayıları 60'ı geçemez")
+                            .foregroundStyle(.red)
+                    }
+                }
+                
+                Section {
+                    Text("2022 KPSS Puanı: \(result2022, specifier: "%.3f")")
+                        .bold()
+                    Text("2023 KPSS Puanı: \(result2023, specifier: "%.3f")")
+                        .bold()
+                    
+                    CalculateButton(title: "Hesapla") {
+                        // genel kültür ve genel yetenek netlerimizi hesaplıyoruz
+                        
+                        let gkNet = gkTrueCount - (gkFalseCount / 4)
+                        let gyNet = gyTrueCount - (gyFalseCount / 4)
+                        
+                        
+                        withAnimation{
+                            result2023 = (51.209 + gyNet * 0.537 + gkNet * 0.418)
+                            result2022 = (48.616 + gyNet * 0.4756 + gkNet * 0.4192)
+                            
+                        }
+                            
+                        
+                        
+                    }
+                    .disabled(formControl)
+                    .sensoryFeedback(.success, trigger: result2022)
+                    
+                    
+                } header: {
+                    Text("Sonuç")
+                        .textCase(.none)
+                }
+            }
         }
-        .navigationTitle("Lisans")
+        .navigationTitle("Lisans (B Grubu)")
+    }
+    
+    
+    var formControl: Bool {
+        if((gyTrueCount + gyFalseCount) > 60 || (gkTrueCount + gkFalseCount) > 60) {
+           return true
+        }
+        return false
     }
 }
 
