4182165567c2d844785326e4ed8af9bbb033116a
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2024-11-17 23:21:49 +0300

    Önlisans sayfası tasarlandı. Hesaplama işlemi yapıldı.

--- KPSS Puan Hesapla/Views/AssociateView.swift
+++ KPSS Puan Hesapla/Views/AssociateView.swift
@@ -9,12 +9,94 @@ import SwiftUI
 
 // Önlisans
 struct AssociateView: View {
+    @State private var gkTrueCount: Double = 60
+    @State private var gkFalseCount: Double = 0
+    
+    @State private var gyTrueCount: Double = 30
+    @State private var gyFalseCount: Double = 0
+    
+    @State private var result: Double = 0
+    
     var body: some View {
         VStack {
             
+            Form {
+                Section {
+                    Stepper("Doğru Sayısı: \(gkTrueCount, specifier: "%.0f")", value: $gkTrueCount, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gkTrueCount)
+                        .bold()
+                    Stepper("Yanlış Sayısı: \(gkFalseCount, specifier: "%.0f")", value: $gkFalseCount, in: 0...60)
+                        .sensoryFeedback(.selection, trigger: gkFalseCount)
+                        .bold()
+                    
+                } header: {
+                    Text("Genel Kültür")
+                        .textCase(.none)
+                }footer: {
+                    if(gkTrueCount + gkFalseCount > 60) {
+                        Text("Toplam doğru ve yanlış sayıları 60'ı geçemez")
+                            .foregroundStyle(.red)
+                    }
+                }
+                
+                Section {
+                    Stepper("Doğru Sayısı: \(gyTrueCount, specifier: "%.0f")", value: $gyTrueCount, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gyTrueCount)
+                        .bold()
+                    Stepper("Yanlış Sayısı: \(gyFalseCount, specifier: "%.0f")", value: $gyFalseCount, in: 0...60)
+                        .sensoryFeedback(.selection, trigger: gyFalseCount)
+                        .bold()
+                    
+                } header: {
+                    Text("Genel Yetenek")
+                        .textCase(.none)
+                }footer: {
+                    if(gyTrueCount + gyFalseCount > 60) {
+                        Text("Toplam doğru ve yanlış sayıları 60'ı geçemez")
+                            .foregroundStyle(.red)
+                    }
+                }
+                
+                Section {
+                    Text("KPSS Puanı: \(result, specifier: "%.3f")")
+                        .bold()
+                    
+                    CalculateButton(title: "Hesapla") {
+                        // genel kültür ve genel yetenek netlerimizi hesaplıyoruz
+                        
+                        let gkNet = gkTrueCount - (gkFalseCount / 4)
+                        let gyNet = gyTrueCount - (gyFalseCount / 4)
+                        
+                        
+                        withAnimation{
+                            result = (53.816 + gyNet * 0.43 + gkNet * 0.397)
+                            
+                        }
+                            
+                        
+                        
+                    }
+                    .disabled(formControl)
+                    .sensoryFeedback(.success, trigger: result)
+                    
+                    
+                } header: {
+                    Text("Sonuç")
+                        .textCase(.none)
+                }
+            }
         }
         .navigationTitle("Önlisans")
     }
+    
+    
+    var formControl: Bool {
+        if((gyTrueCount + gyFalseCount) > 60 || (gkTrueCount + gkFalseCount) > 60) {
+           return true
+        }
+        return false
+    }
+    
 }
 
 #Preview {
