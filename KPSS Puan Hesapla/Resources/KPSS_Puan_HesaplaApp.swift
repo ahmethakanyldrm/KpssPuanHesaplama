01131711bb1ca3213730ca181196f6eada9dbd71
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2024-11-17 22:06:22 +0300

    Scroll ve default durumunda tabbar ve navbarın davranışı belirlendi

--- KPSS Puan Hesapla/Resources/KPSS_Puan_HesaplaApp.swift
+++ KPSS Puan Hesapla/Resources/KPSS_Puan_HesaplaApp.swift
@@ -9,6 +9,22 @@ import SwiftUI
 
 @main
 struct KPSS_Puan_HesaplaApp: App {
+    
+    init() {
+        // scroll ve default durumda navbarın nasıl davranacağını belirleme
+        let appearanceNav = UINavigationBarAppearance()
+        appearanceNav.configureWithOpaqueBackground()
+        UINavigationBar.appearance().standardAppearance = appearanceNav
+        UINavigationBar.appearance().scrollEdgeAppearance = appearanceNav
+        
+        // scroll ve default durumda tabbarın nasıl davranacağını belirleme
+
+        let appearanceTab = UITabBarAppearance()
+        appearanceTab.configureWithOpaqueBackground()
+        UITabBar.appearance().scrollEdgeAppearance = appearanceTab
+        UITabBar.appearance().standardAppearance = appearanceTab
+    }
+    
     var body: some Scene {
         WindowGroup {
             RootView()
