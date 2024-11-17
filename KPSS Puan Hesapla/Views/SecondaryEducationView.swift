d949415d21a66f15f451b4517247d203ad34a37e
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2024-11-17 22:31:37 +0300

    Ortaöğretim Hesaplama sayfası tasarlandı.

--- KPSS Puan Hesapla/Views/SecondaryEducationView.swift
+++ KPSS Puan Hesapla/Views/SecondaryEducationView.swift
@@ -9,9 +9,53 @@ import SwiftUI
 
 // Ortaöğretim
 struct SecondaryEducationView: View {
+    @State private var gkTrueCount = 30
+    @State private var gkFalseCount = 0
+    
+    @State private var gyTrueCount = 30
+    @State private var gyFalseCount = 0
+    
+    @State private var result = 0
+    
     var body: some View {
         VStack {
             
+            Form {
+                Section {
+                    Stepper("Doğru Sayısı: \(gkTrueCount)", value: $gkTrueCount, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gkTrueCount)
+                    Stepper("Yanlış Sayısı: \(gkFalseCount)", value: $gkFalseCount, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gkFalseCount)
+                    
+                } header: {
+                    Text("Genel Kültür")
+                        .textCase(.none)
+                }
+                
+                Section {
+                    Stepper("Doğru Sayısı: \(gyTrueCount)", value: $gyTrueCount, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gyTrueCount)
+                    Stepper("Yanlış Sayısı: \(gyFalseCount)", value: $gyFalseCount, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gyFalseCount)
+                    
+                } header: {
+                    Text("Genel Yetenek")
+                        .textCase(.none)
+                }
+                
+                Section {
+                    Text("KPSS Puanı: \(result)")
+                    
+                    CalculateButton(title: "Hesapla") {
+                        
+                    }
+                    
+                    
+                } header: {
+                    Text("Sonuç")
+                        .textCase(.none)
+                }
+            }
         }
         .navigationTitle("Ortaöğretim")
     }
