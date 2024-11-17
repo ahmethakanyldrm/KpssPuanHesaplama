9ebab7222c685e2bdb40baa3f33d98b808d60337
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2024-11-17 22:57:57 +0300

    Ortaöğretim KPSS Puan hesaplama fonksiyonu yazıldı ve butona basıldığında hesaplama yapması sağlandı.Hatalı girişlerde uyarı mesajı ekrana yazdırıldı

--- KPSS Puan Hesapla/Views/SecondaryEducationView.swift
+++ KPSS Puan Hesapla/Views/SecondaryEducationView.swift
@@ -9,46 +9,63 @@ import SwiftUI
 
 // Ortaöğretim
 struct SecondaryEducationView: View {
-    @State private var gkTrueCount = 30
-    @State private var gkFalseCount = 0
+    @State private var gkTrueCount: Double = 60
+    @State private var gkFalseCount: Double = 0
     
-    @State private var gyTrueCount = 30
-    @State private var gyFalseCount = 0
+    @State private var gyTrueCount: Double = 30
+    @State private var gyFalseCount: Double = 0
     
-    @State private var result = 0
+    @State private var result: Double = 0
     
     var body: some View {
         VStack {
             
             Form {
                 Section {
-                    Stepper("Doğru Sayısı: \(gkTrueCount)", value: $gkTrueCount, in: 1...60)
+                    Stepper("Doğru Sayısı: \(gkTrueCount, specifier: "%.0f")", value: $gkTrueCount, in: 1...60)
                         .sensoryFeedback(.selection, trigger: gkTrueCount)
-                    Stepper("Yanlış Sayısı: \(gkFalseCount)", value: $gkFalseCount, in: 1...60)
+                    Stepper("Yanlış Sayısı: \(gkFalseCount, specifier: "%.0f")", value: $gkFalseCount, in: 0...60)
                         .sensoryFeedback(.selection, trigger: gkFalseCount)
                     
                 } header: {
                     Text("Genel Kültür")
                         .textCase(.none)
+                }footer: {
+                    if(gkTrueCount + gkFalseCount > 60) {
+                        Text("Toplam doğru ve yanlış sayıları 60'ı geçemez")
+                            .foregroundStyle(.red)
+                    }
                 }
                 
                 Section {
-                    Stepper("Doğru Sayısı: \(gyTrueCount)", value: $gyTrueCount, in: 1...60)
+                    Stepper("Doğru Sayısı: \(gyTrueCount, specifier: "%.0f")", value: $gyTrueCount, in: 1...60)
                         .sensoryFeedback(.selection, trigger: gyTrueCount)
-                    Stepper("Yanlış Sayısı: \(gyFalseCount)", value: $gyFalseCount, in: 1...60)
+                    Stepper("Yanlış Sayısı: \(gyFalseCount, specifier: "%.0f")", value: $gyFalseCount, in: 0...60)
                         .sensoryFeedback(.selection, trigger: gyFalseCount)
                     
                 } header: {
                     Text("Genel Yetenek")
                         .textCase(.none)
+                }footer: {
+                    if(gyTrueCount + gyFalseCount > 60) {
+                        Text("Toplam doğru ve yanlış sayıları 60'ı geçemez")
+                            .foregroundStyle(.red)
+                    }
                 }
                 
                 Section {
-                    Text("KPSS Puanı: \(result)")
+                    Text("KPSS Puanı: \(result, specifier: "%.3f")")
                     
                     CalculateButton(title: "Hesapla") {
+                        // genel kültür ve genel yetenek netlerimizi hesaplıyoruz
+                        
+                        let gkNet = gkTrueCount - (gkFalseCount / 4)
+                        let gyNet = gyTrueCount - (gyFalseCount / 4)
+                        
+                        result = 55.839 + gyNet * 0.348 + gkNet * 0.431
                         
                     }
+                    .disabled(formControl)
                     
                     
                 } header: {
@@ -59,6 +76,15 @@ struct SecondaryEducationView: View {
         }
         .navigationTitle("Ortaöğretim")
     }
+    
+    
+    var formControl: Bool {
+        if((gyTrueCount + gyFalseCount) > 60 || (gkTrueCount + gkFalseCount) > 60) {
+           return true
+        }
+        return false
+    }
+    
 }
 
 #Preview {
